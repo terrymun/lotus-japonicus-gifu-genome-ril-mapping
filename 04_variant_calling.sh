72055c67e4b831d50bf67e659628aad183e54000
Terry Mun <hello@terrymun.com>
2020-04-25 02:27:14 +0200

    Removed RIL folder from config
    This is to avoid confusion as the RIL folder should not be
    configurable, but simply set by the outputDirectory variable.

--- 04_variant_calling.sh
+++ 04_variant_calling.sh
@@ -7,10 +7,6 @@
 # Output directory
 outputDirectory="/path/to/output_directory/"
 
-# Individual folders for the output of each RIL population
-gifuBurtiiOutputDirectory="${outputDirectory}/Gifu-Burtii"
-gifuMG20OutputDirectory="${outputDirectory}/Gifu-MG20"
-
 # FASTA file of preliminary assembled genome
 genomeFastaFile="/path/to/prelimiary_genome_fasta_file.fa"
 
@@ -20,24 +16,24 @@ genomeFastaFile="/path/to/prelimiary_genome_fasta_file.fa"
 
 
 # Make folders for each RIL population
-`mkdir -p $gifuBurtiiOutputDirectory`
-`mkdir -p $gifuMG20OutputDirectory`
+`mkdir -p ${outputDirectory}/Gifu-Burtii`
+`mkdir -p ${outputDirectory}/Gifu-MG20`
 
 # Create bam file list containing the biparental bam files for each population
-echo "${outputDirectory}/Gifu/Gifu.picardDeduped.bam" > "${gifuBurtiiOutputDirectory}/bamFileList.txt"
-echo "${outputDirectory}/Burttii/Burttii.merged.bam" >> "${gifuBurtiiOutputDirectory}/bamFileList.txt"
+echo "${outputDirectory}/Gifu/Gifu.picardDeduped.bam" > "${outputDirectory}/Gifu-Burtii/bamFileList.txt"
+echo "${outputDirectory}/Burtii/Burtii.merged.bam" >> "${outputDirectory}/Gifu-Burtii/bamFileList.txt"
 
-echo "${outputDirectory}/Gifu/Gifu.picardDeduped.bam" > "${gifuMG20OutputDirectory}/bamFileList.txt"
-echo "${outputDirectory}/MG20/MG20.merged.bam" >> "${gifuMG20OutputDirectory}/bamFileList.txt"
+echo "${outputDirectory}/Gifu/Gifu.picardDeduped.bam" > "${outputDirectory}/Gifu-MG20/bamFileList.txt"
+echo "${outputDirectory}/MG20/MG20.merged.bam" >> "${outputDirectory}/Gifu-MG20/bamFileList.txt"
 
 # Generate contig regions to perform mpileup in parallel
 regions=`cat $genomeFastaFile | grep "^>" | sed 's/>//' |  cut -d ' ' -f1 | sort -n | uniq`
 
-# Gifu x Burttii RILs start with C<...>
-`find $outputDirectory -type f -name "C*.merged.bam" | sort >> $gifuBurtiiOutputDirectory/bamFileList.txt`
+# Gifu x Burtii RILs start with C<...>
+`find $outputDirectory -type f -name "C*.merged.bam" | sort >> ${outputDirectory}/Gifu-Burtii/bamFileList.txt`
 
 # Gifu x MG20 RILs start with R<...>
-`find $outputDirectory -type f -name "R*.merged.bam" | sort >> $gifuMG20OutputDirectory/bamFileList.txt`
+`find $outputDirectory -type f -name "R*.merged.bam" | sort >> ${outputDirectory}/Gifu-MG20/bamFileList.txt`
 
 # Perform mpileup for each RIL population
 for region in $regions; do
